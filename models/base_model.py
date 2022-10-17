46097fa5deeadbade0d74971acb1f09ae0f26f48
UNICA-FIME <juancarlosfime@gmail.com>
2022-10-17 21:18:21 +0000

    object

--- models/base_model.py
+++ models/base_model.py
@@ -19,9 +19,21 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if (len(kwargs) > 0):
+            k = kwargs.keys()
+            for i in k:
+                if i[0] == "_":
+                    key_1 = i
+            kwargs.pop(key_1)
+            self.__dict__ = kwargs
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """This is a function what represents the
